7d2da84464b1c24cd73a0b21679b194df24c7a8e
Pierre Delestre <ADM_DelestrePr@gial.be>
2023-07-15 20:13:13 +0200

    First commit - use of Linq

--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -1,7 +1,21 @@
 ﻿using HRAdministrationAPI;
+using System.Linq;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+decimal totalSalaries = 0;
+List<IEmployee> employees = new List<IEmployee>();
+
+SeedData(employees);
+
+//foreach (var employee in employees)
+//{
+//    totalSalaries += employee.Salary;
+//}
+
+//Console.WriteLine($"Total annual salaries (including bonus): {totalSalaries}");
+
+Console.WriteLine($"Total annual salaries (including bonus): {employees.Sum(e => e.Salary)}");
+Console.ReadKey();
 
 static void SeedData(List<IEmployee> employees)
 {
@@ -35,25 +49,46 @@ static void SeedData(List<IEmployee> employees)
 
     employees.Add(headOfDepartment);
 
+    IEmployee deputyHeadMaster = new DeputyHeadMaster
+    {
+        ID = 4,
+        FirstName = "Devlin",
+        LastName = "Brown",
+        Salary = 60000
+    };
 
+    employees.Add(deputyHeadMaster);
+
+    IEmployee headMaster = new HeadMaster
+    {
+        ID = 5,
+        FirstName= "Damien",
+        LastName = "Jones",
+        Salary = 80000
+    };
+
+    employees.Add(headMaster);
 }
 
 public class Teacher: EmployeeBase
 {
-
+    public override decimal Salary { get => base.Salary + (base.Salary * 0.02m); set => base.Salary = value; }
 }
 
 public class HeadOfDepartment : EmployeeBase
 {
+    public override decimal Salary { get => base.Salary + (base.Salary * 0.03m); set => base.Salary = value; }
 
 }
 
 public class DeputyHeadMaster : EmployeeBase
 {
+    public override decimal Salary { get => base.Salary + (base.Salary * 0.04m); set => base.Salary = value; }
 
 }
 
 public class HeadMaster : EmployeeBase
 {
+    public override decimal Salary { get => base.Salary + (base.Salary * 0.05m); set => base.Salary = value; }
 
 }
